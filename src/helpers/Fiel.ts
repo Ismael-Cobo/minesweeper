b805a1f493c1f17d1fffa5f7390c7d9cca574509
Ismael Cobo <midismael@gmail.com>
2022-05-11 18:20:43 +0200

    feat: logic for minesweeper

--- src/helpers/Fiel.ts
+++ src/helpers/Fiel.ts
@@ -1,6 +1,10 @@
+import { incrementNeibours } from './CellsManipulator';
+
 export type Cell = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 
 export type Field = Cell[][];
+
+export type Coords = [Cell, Cell]
 
 export const CellState: Record<string, Cell> = {
   empty: 0,
@@ -14,24 +18,34 @@ export const emptyFieldGenerator = (size: number, state: Cell = CellState.empty)
   return new Array(size).fill(null).map(() => new Array(size).fill(state));
 };
 
-export const fieldGenerator = (size: number, dencity: number): Field => {
+export const fieldGenerator = (size: number, probability: number): Field => {
 
-  if (dencity < 0 || dencity > 1) {
-    throw new Error("Dencity must be between 0 to 1");
+  if (probability < 0 || probability > 1) {
+    throw new Error("Probability must be between 0 to 1");
   }
 
-  const freeCellsCount = size * size
-  const cellsWithBombs = freeCellsCount * dencity
+  let unprocessedCells = size * size
+  let restCellsWithBombs = unprocessedCells * probability
 
   const result: Field = emptyFieldGenerator(size)
 
   for (let i = 0; i < size; i++) {
-    for (let j = 0; j < size; j++) {
-      if (cellsWithBombs === 0) {
+    for (let j:Cell = 0; j < size; j++) {
+      if (restCellsWithBombs === 0) {
         return result
       }
+
+      if(restCellsWithBombs / unprocessedCells > Math.random()) {
+        result[i][j] = CellState.bomb
+        incrementNeibours([i as Cell, j as Cell], result)
+        restCellsWithBombs--;
+      }
+
+      unprocessedCells--
+
     }
   }
+  console.log(result, '---------------')
 
   return result
 }
