218226e986c8f5808c145b0cfc915f8a68fd69ed
Ismael Cobo <midismael@gmail.com>
2022-05-16 17:15:00 +0200

    feat: scoreboard added

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,31 @@
 import './App.css';
 import { Header } from './components/Header/Header';
-import { Level } from './components/Scoreboard/Level/Level';
+import { Scoreboard } from './components/Scoreboard';
 
 function App() {
 
   return (
     <div className="App">
-      <Header
-        feature='flag'
-        firstAction='alt'
-        secondAction='click'
-      >
-        Minesweeper
-      </Header>
-      <Level children={['easy', 'medium', 'expert']} />
+      <>
+        <Header
+          feature='flag'
+          firstAction='alt'
+          secondAction='click'
+        >
+          Minesweeper
+        </Header>
+        <br />
+      </>
+      <Scoreboard
+        numberOfBoms='010'
+        dificulty={['easy', 'medium', 'expert']}
+        onReset={() => null}
+        timer='000'
+      />
+
     </div>
   );
 }
+
 
 export default App;
